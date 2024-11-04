5595372346bc2b08b54b2822cc00a856bbd8d58d
Pritish Togare <944625+pritish27@users.noreply.github.com>
2024-11-04 19:34:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
